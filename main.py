b4138f092ce0064113de2e72116aa28fc7898244
Intrum1 <132203102+Intrum1@users.noreply.github.com>
2024-02-15 18:14:58 +0100

    Впрва3

--- main.py
+++ main.py
@@ -7,27 +7,32 @@ class TemperatureApp:
         self.master.title("Метереологічні дослідження")
         self.master.geometry("400x250")
 
-        self.canvas = Canvas(self.master, width=290, height=250)
+        # Створення полотна
+        self.canvas = Canvas(master, width=290, height=250)
         self.canvas.place(x=110, y=0)
 
-        self.Lbl_temperature = Label(self.master, text="Температура")
+        # Створення елементів керування
+        self.Lbl_temperature = Label(master, text="Температура")
         self.Lbl_temperature.place(x=10, y=10)
 
-        self.Lbox = Listbox(self.master)
+        self.Lbox = Listbox(master)
         self.Lbox.place(x=10, y=40)
 
-        self.Btn_build = Button(self.master, text="Побудувати", command=self.build_random_temperatures)
+        self.Btn_build = Button(master, text="Побудувати", command=self.generate_temperatures)
         self.Btn_build.place(x=10, y=210)
 
-    def build_random_temperatures(self):
-
+    def generate_temperatures(self):
+        # Очистка Listbox перед додаванням нових значень
         self.Lbox.delete(0, END)
 
-        for _ in range(7):
-            temperature = randint(15, 25)
-            self.Lbox.insert(END, temperature)
+        # Генеруємо 7 нових випадкових чисел
+        temperatures = [randint(15, 25) for _ in range(7)]
 
+        # Додавання згенерованих значень до Listbox
+        for temp in temperatures:
+            self.Lbox.insert(END, temp)
+
+# Створення головного вікна
 root = Tk()
 app = TemperatureApp(root)
-
 root.mainloop()
