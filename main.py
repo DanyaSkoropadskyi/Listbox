111c4f5089565c75a126331d4bce43170385b94c
Intrum1 <132203102+Intrum1@users.noreply.github.com>
2024-02-22 19:01:31 +0100

    11

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import random
 
 class TemperatureApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Температурна програма")
+        self.root.title("Метереологічні дослідження")
         self.root.geometry("500x400")
 
         self.temperatures = []
@@ -23,7 +23,8 @@ class TemperatureApp:
 
         self.fig, self.ax = plt.subplots()
         self.canvas = FigureCanvasTkAgg(self.fig, master=root)
-        self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        self.canvas_widget = self.canvas.get_tk_widget()
+        self.canvas_widget.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
     def build_graph(self):
         self.ax.clear()  # Очистка графіка перед побудовою нового
@@ -41,21 +42,10 @@ class TemperatureApp:
             self.ax.plot(days[self.temperatures.index(max_temp)], max_temp, marker='o', markersize=8, color='red')
             self.ax.plot(days[self.temperatures.index(min_temp)], min_temp, marker='o', markersize=8, color='green')
 
-            # Позначення точок максимальних та мінімальних температур колами
-            self.ax.scatter(days[self.temperatures.index(max_temp)], max_temp, color='red', s=100, zorder=3)
-            self.ax.scatter(days[self.temperatures.index(min_temp)], min_temp, color='green', s=100, zorder=3)
-
             # Налаштування графіка
             self.ax.set_xlabel('Дні тижня')
             self.ax.set_ylabel('Температура, °C')
             self.ax.set_title('Температури протягом тижня')
-
-            # Додавання сітки та написів на осі
-            self.ax.grid(True)
-            self.ax.set_xticks(days)
-            self.ax.set_yticks(range(min(self.temperatures), max(self.temperatures) + 1))
-            self.ax.set_xticklabels(days)
-            self.ax.set_yticklabels(range(min(self.temperatures), max(self.temperatures) + 1))
 
             # Оновлення графіка
             self.canvas.draw()
